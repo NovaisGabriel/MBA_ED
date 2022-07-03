1e1692b357ebf00638535da9d816a6539d22267d
gabrielnovais <gabrieel.novais@gmail.com>
2022-07-03 10:42:54 -0300

    Teste 15, modificando parâmetros, ajustes em functions

--- infra/aws/function.tf
+++ infra/aws/function.tf
@@ -1,6 +1,7 @@
 resource "aws_lambda_function" "decompresss" {
   filename      = "../../functions/fn_extract_rais.zip"
-  function_name = "${local.prefix}_igti_df_extract_rais"
+  # function_name = "${local.prefix}_igti_df_extract_rais"
+  function_name = "decompresss"
   role          = aws_iam_role.lambda_decompress.arn
   handler       = "handler.handler"
   runtime       = "python3.8"
