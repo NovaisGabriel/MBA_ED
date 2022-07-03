ec30a7c087a8e113555ee176d20c751351425b32
gabrielnovais <gabrieel.novais@gmail.com>
2022-07-03 09:19:57 -0300

    Teste 13, novo, ajustes em functions

--- infra/aws/function.tf
+++ infra/aws/function.tf
@@ -3,9 +3,11 @@ resource "aws_lambda_function" "decompresss3" {
   function_name = "${local.prefix}_igti_df_extract_rais"
   role          = aws_iam_role.lambda_decompress.arn
   handler       = "handler.handler"
+  # teste 
   runtime     = "python3.8"
   timeout     = 900
   memory_size = 10000
+  # Teste 
   tags = local.common_tags
 
 }
